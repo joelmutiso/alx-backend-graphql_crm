9b5d13120d65d520e439c576fb24a1e8f72c35e6
Joel Mutiso <joelwmutiso@gmail.com>
2026-02-04 02:31:02 +0300

    feat: implement heartbeat logger with django-crontab

--- crm/cron.py
+++ crm/cron.py
@@ -1,12 +1,18 @@
 from datetime import datetime
+from gql import gql, Client
+from gql.transport.requests import RequestsHTTPTransport
 
 def log_crm_heartbeat():
-    # Get current time in the specific format: DD/MM/YYYY-HH:MM:SS
-    timestamp = datetime.now().strftime("%d/%m/%Y-%H:%M:%S")
-    
-    # Construct the message
-    message = f"{timestamp} CRM is alive\n"
-    
-    # Append to the log file (mode 'a' for append)
-    with open('/tmp/crm_heartbeat_log.txt', 'a') as f:
-        f.write(message)
+    try:
+        transport = RequestsHTTPTransport(url='http://localhost:8000/graphql/')
+        client = Client(transport=transport, fetch_schema_from_transport=True)
+        query = gql('query { hello }')
+        client.execute(query)
+        
+        timestamp = datetime.now().strftime("%d/%m/%Y-%H:%M:%S")
+        message = f"{timestamp} CRM is alive\n"
+        
+        with open('/tmp/crm_heartbeat_log.txt', 'a') as f:
+            f.write(message)
+    except Exception:
+        pass
